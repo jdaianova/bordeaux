cf6ac80e73c0e3696dbe1e0b228ebd5b0aafec03
Julia <jdaianova@gmail.com>
2023-05-06 15:44:36 +0200

    1 commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { data } from './data';
 
 function App() {
+  const [places, setPlaces] = useState(data);
+  const [showAllPlaces, setShowAllPlaces] = useState(true);
+  const [btnText, setBtnText] = useState('clear all');
+  const [showText, setShowText] = useState(true);
+
+  const detelePlace = (id) => {
+    let currentPlaces = places.filter(el => el.id !== id);
+    setPlaces(currentPlaces);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header className='Header'>
+        <h1>Bordeaux</h1>
+        <a href='#TitleGallery' className='ChoosePlaces'>Choose your places to visit</a>
       </header>
+
+      <div className='TitleGallery'>
+        <h2 id='TitleGallery'>You can see in Bordeaux {places.length} amazing places:</h2>
+      </div>
+
+      <div className='Gallery'>
+        {places.map((el) => {
+          const { id, title, image, description, website, showMore } = el;
+
+          return (
+            <div className='Place' key={id}>
+              <div className='TitlePlace'>{title}</div>
+
+              <div className='ImagePlace'>
+                <img src={image} width='250px' alt={title}></img>
+              </div>
+
+              <div className='DescriptionPlace' onClick={() => {
+                el.showMore = !el.showMore;
+                setShowText(!showText);
+              }}>
+                {showMore ? description : description.substring(0, 65)}
+                <button className='BtnShowMore'>{showMore ? '... show less' : '... show more'}</button>
+              </div>
+
+              <div className='LinkPlace'>
+                <a href={website} target="_blank">Find out more about this place &raquo; </a>
+              </div>
+
+              <button
+                className='BtnDeletePlace'
+                onClick={() => detelePlace(id)}>
+                don't want to visit
+              </button>
+            </div>
+          )
+        })}
+      </div>
+
+      <button
+        className='BtnClearAll'
+        onClick={() => {
+          showAllPlaces ? setPlaces([]) : setPlaces(data);
+          showAllPlaces ? setBtnText('return all') : setBtnText('clear all');
+          setShowAllPlaces(!showAllPlaces);
+        }}>
+        {btnText}
+      </button>
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,87 @@
+import museumVin from './img/museum La Cite du Vin.jpeg';
+import placeBourse from './img/souvenirs-de-mes-balades.jpg';
+import chateau from './img/chateau-pape-clement.jpg';
+import cathedral from './img/cathedral.jpg';
+import PontDePierre from './img/PontDePierre.jpg';
+import PorteCailhau from './img/PorteCailhau.jpg';
+
+export const data = [
+    {
+        id: 1,
+
+        title: 'Museum La Cite du Vin',
+
+        image: museumVin,
+
+        description: "La Cité du Vin is a unique cultural facility where wine comes to life through an immersive, sensorial approach, all set within an evocative architectural design. La Cité du Vin gives a different view of wine, across the world, across the ages, across all cultures and all civilisations. A must-see experience during any stay in Bordeaux.",
+
+        website: 'https://www.laciteduvin.com/en',
+
+        showMore: false,
+    },
+    {
+        id: 2,
+
+        title: 'Place de la Bourse',
+
+        image: placeBourse,
+
+        description: "Place de la Bourse is a square in Bordeaux, France and one of the city's most recognisable sights. Built from 1730 to 1775 along the river Garonne, it was a multi-building development designed by architect Ange-Jacques Gabriel.",
+
+        website: 'https://www.bordeaux.fr/l3293',
+
+        showMore: false,
+    },
+    {
+        id: 3,
+
+        title: 'Chateau Pape Clement',
+
+        image: chateau,
+
+        description: "15 minutes far from Bordeaux downtown, Château Pape Clément is a good example of an urban vineyard, as its well-known neighbor Château Haut-Brion. This is one of the oldest Grands Crus in the whole Bordeaux region. The vineyard was planted in the 13th century by Bertrand de Goth, who went on to become Pope Clement V. The estate prides itself on having produced wine on the same land for 7 centuries. Rich in history, Château Pape Clément can also be proud of its beautiful park, architecture and arts, as the beautiful glasshouse built by Gustave Eiffel.",
+
+        website: 'https://www.chateau-pape-clement.fr/en',
+
+        showMore: false,
+    },
+    {
+        id: 4,
+
+        title: 'St. Andre Cathedral',
+
+        image: cathedral,
+
+        description: "Bordeaux Cathedral, officially known as the Primatial Cathedral of St Andrew of Bordeaux (French: Cathédrale-Primatiale Saint-André de Bordeaux), is a Catholic church dedicated to Saint Andrew and located in Bordeaux, France. It is the seat of the Archbishop of Bordeaux. In 1998, UNESCO designated the Routes of Santiago de Compostela in France as a World Heritage site, including the three main churches of Bordeaux: the basilica of St Severinus, the basilica of St Michael and the cathedral of St Andrew.",
+
+        website: 'https://www.bordeaux-tourism.co.uk/cultural-heritage/cathedrale-saint-andre.html',
+    
+        showMore: false,
+    },
+    {
+        id: 5,
+
+        title: 'Pont de Pierre',
+
+        image: PontDePierre,
+
+        description: "The Pont de pierre, or Stone Bridge in English, is a bridge in Bordeaux, (in the Gironde department of France), which connects the left bank of the river Garonne (cours Victor Hugo) to the right bank quartier de la Bastide (Avenue Thiers). It is 487 m (1,598 ft) in length and 19 m (62 ft) wide.",
+
+        website: 'https://www.bordeaux-tourisme.com/patrimoine-culturel/pont-pierre.html',
+    
+        showMore: false,
+    },
+    {
+        id: 6,
+
+        title: 'Porte Cailhau',
+
+        image: PorteCailhau,
+
+        description: "The Port Cailhau, thirty-five metres tall, was integrated into the city walls. In 1864, it was rented by a public letter writer and a person whose job was to weigh salt. They were both evicted in order to renovate the monument. There is a magnificent view of the oldest bridge in Bordeaux, the Pont de Pierre, from here.",
+
+        website: 'https://www.visiter-bordeaux.com/en/discovering-bordeaux/porte-cailhau.html',
+    
+        showMore: false,
+    },
+]
